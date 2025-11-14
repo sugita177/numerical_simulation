3eace9ac8e933dabbfe79a77f3b55bbbdf10d1c6
Sugita <moriaki.sugita.1730@gmail.com>
2025-11-15 01:28:10 +0900

    静的解析の警告を解消

--- src/ordinary_differential_equation/logistic_equation.py
+++ src/ordinary_differential_equation/logistic_equation.py
@@ -1,21 +1,24 @@
 # logistic equation
 # runge_kutta_method
 
-import  numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
+
 
 def logistic(x: float, r: float = 1.2, K: float = 3.0) -> float:
     return r * x * (1 - x / K)
 
-def exact_ans(t : float, x0: float, r: float = 1.2, K: float = 3.0) -> float:
+
+def exact_ans(t: float, x0: float, r: float = 1.2, K: float = 3.0) -> float:
     return K / (1.0 + (K / x0 - 1.0) * np.exp(-r * t))
 
+
 N: int = 20000
-h : float = 1.0e-3
-t : float = 0
+h: float = 1.0e-3
+t: float = 0
 t_list = np.array([])
-x0 : float = 0.1
-x : float = x0
+x0: float = 0.1
+x: float = x0
 x_list = np.array([])
 exac_list = np.array([])
 
@@ -41,7 +44,7 @@ plt.ylabel("number of indivisuals")
 plt.ylim(-1.0, 4.0)
 
 plt.grid(True)
-plt.plot(t_list, exac_list, color = "k", label = "exact_ans")
-plt.scatter(t_list, x_list, color = "b", label = "Runge Kutta", marker="o")
+plt.plot(t_list, exac_list, color="k", label="exact_ans")
+plt.scatter(t_list, x_list, color="b", label="Runge Kutta", marker="o")
 plt.legend()
 plt.show()
